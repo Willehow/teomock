c3e67ad2ee3e456e34ff874ed89a1087fd72614c
André Lima <37313063+Willehow@users.noreply.github.com>
2019-07-01 19:32:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/proponentesPorCPF', function (req, res) {
 
 app.post('/login', (req, res) => {
  
-  var cpf = req.body.username;
+  var cpf = req.body.username.replace(/\D/g, '');
 
   if(cpf == "00000000001" || cpf == "00000000002" || cpf == "00000000003"){
 	  
