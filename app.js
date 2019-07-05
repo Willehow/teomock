a52e6cc4c2b9beebb35e7a109c641e3a9d93fc92
André Lima <37313063+Willehow@users.noreply.github.com>
2019-07-05 15:06:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,18 +18,55 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/proponentesPorCPF', function (req, res) {
-  res.send({
-	"cpf":"00000000001",
-  	"nome": "Jonelson"
-  });
+app.get('/proposta/:cpf', function (req, res) {
+
+  switch(req.params.cpf) {
+    case "00000000002":
+      // code block
+      res.send({
+        "cpf":"00000000002",
+        "nome": "Jonelson",
+        "contribuicao": {
+          "percentual": "8%",
+          "valor": "R$200"
+        }
+      });
+      break;
+    case "00000000003":
+      // code block
+      res.send({
+        "cpf":"00000000003",
+        "nome": "Joilson",
+        "contribuicao": {
+          "percentual": "8%",
+          "valor": "R$300"
+        }
+      });
+      break;
+    case "00000000004":
+      // code block
+      res.send({
+        "cpf":"00000000004",
+        "nome": "Jow",
+        "contribuicao": {
+          "percentual": "8%",
+          "valor": "R$500"
+        }
+      });
+      break;
+    default:
+      // code block
+      res.send("Não encontrado");
+  }
+
 });
 
 app.post('/login', (req, res) => {
 
   var cpf = req.body.username.replace(/\D/g, '');
+  var password = req.body.password;
 
-  if (cpf == "00000000001" || cpf == "00000000002" || cpf == "00000000003") {
+  if ((cpf == "00000000002" || cpf == "00000000003" || cpf == "00000000004") && password == "20182018") {
 
     res.sendStatus(200);
   } else {
